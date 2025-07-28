54b1c407ef4d1ece400bd4e48e21b4eba5132cf2
Artem <aklimichevm@yandex.ru>
2025-07-28 14:20:46 +0300

    asd

--- cipher.c
+++ cipher.c
@@ -1,10 +1,11 @@
-#include <limits.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
 
 int main() {
     int choice;
-    char path[PATH_MAX];
+    char path[PATH_MAX] = "";
+    int file_loaded = 0;
 
     while (1) {
         if (scanf("%d", &choice) != 1) {
@@ -12,10 +13,15 @@ int main() {
             return 0;
         }
 
+        getchar();
+
         if (choice == -1) {
             return 0;
         } else if (choice == 1) {
-            scanf("%s", path);
+            if (scanf("%s", path) != 1) {
+                printf("n/a\n\n");
+                continue;
+            }
 
             FILE *file = fopen(path, "r");
             if (file == NULL) {
@@ -23,9 +29,53 @@ int main() {
                 continue;
             }
 
-            int ch;
-            int isEmpty = 1;
+            int ch, isEmpty = 1;
+            while ((ch = fgetc(file)) != EOF) {
+                putchar(ch);
+                isEmpty = 0;
+            }
 
+            fclose(file);
+
+            if (isEmpty) {
+                printf("n/a");
+            } else {
+                file_loaded = 1;
+            }
+
+            printf("\n\n");
+
+        } else if (choice == 2) {
+            if (!file_loaded) {
+                printf("n/a\n\n");
+                continue;
+            }
+
+            char buffer[1024];
+
+            if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+                printf("n/a\n\n");
+                continue;
+            }
+
+            buffer[strcspn(buffer, "\n")] = '\0';
+
+            FILE *file = fopen(path, "a");
+            if (file == NULL) {
+                printf("n/a\n\n");
+                continue;
+            }
+
+            fprintf(file, "%s\n", buffer);
+            fclose(file);
+
+            file = fopen(path, "r");
+            if (file == NULL) {
+                printf("n/a\n\n");
+                continue;
+            }
+
+            int ch, isEmpty = 1;
             while ((ch = fgetc(file)) != EOF) {
                 putchar(ch);
                 isEmpty = 0;
@@ -38,6 +88,7 @@ int main() {
             }
 
             printf("\n\n");
+
         } else {
             printf("n/a\n\n");
         }
